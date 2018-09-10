a2286ad15db047301863cff6c7badc375406c17c
Linus Berg <linus@fenix.me.uk>
2018-09-10 13:51:26 +0200

    I am retarded.

--- c/algo_1.c
+++ c/algo_1.c
@@ -1,42 +1,15 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-typedef struct char_node {
-  char c;
-  /* We print backwards. */
-  struct char_node *prev_node;
-} char_node;
-
-char_node *ReadC();
-void PrintText();
+void ReadTest(char input);
 
 int main() {
-  char_node *node = ReadC(NULL);
-  /* Currently a memory leak, never free malloc memory, but fuck it. */
-  PrintText(node);
+  ReadTest(getchar());
   return 0;
 }
 
-/* Print text from nodes. */
-void PrintText(char_node *node) {
-  while (node != NULL) {
-    putchar(node->c);
-    node = node->prev_node;
-  }
+void ReadTest(char input) {
+  (input != '\n' ? ReadTest(getchar()) : NULL);
+  putchar(input);
 }
-
-/* Read char. */
-char_node *ReadC(char_node *prev_node) {
-  char input = getchar();
-  /* If it is not a new line keep going. */
-  if (input != '\n') {
-    char_node *node = malloc(sizeof(char_node));
-    node->prev_node = prev_node;
-    node->c = input;
-    return ReadC(node);
-  }
-  /* If a new line is achieved return the previous node. */
-  return prev_node;
-}
-
 
