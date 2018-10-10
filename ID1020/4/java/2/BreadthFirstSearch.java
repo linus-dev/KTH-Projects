00f29456f88b857f862173d34714a65ce8fa004d
Linus Berg <linus@fenix.me.uk>
2018-10-10 13:19:10 +0200

    I AM DONE

--- ID1020/4/java/2/BreadthFirstSearch.java
+++ ID1020/4/java/2/BreadthFirstSearch.java
@@ -1,75 +1,62 @@
 import edu.princeton.cs.algs4.*;
 
 public class BreadthFirstSearch {
+  private ST<String, String> prev = new ST<String, String>();
+  private ST<String, Integer> dist = new ST<String, Integer>();
 
-    // prev[v] = previous vertex on shortest path from s to v
-    // dist[v] = length of shortest path from s to v
-    private ST<String, String>  prev = new ST<String, String>();
-    private ST<String, Integer> dist = new ST<String, Integer>();
+  public BreadthFirstSearch(Graph G, String s) {
+    Queue<String> queue = new Queue<String>();
+    queue.enqueue(s);
+    dist.put(s, 0);
 
-    // run BFS in graph G from given source vertex s
-    public BreadthFirstSearch(Graph G, String s) {
-
-        // put source on the queue
-        Queue<String> queue = new Queue<String>();
-        queue.enqueue(s);
-        dist.put(s, 0);
-        
-        // repeated remove next vertex v from queue and insert
-        // all its neighbors, provided they haven't yet been visited
-        while (!queue.isEmpty()) {
-            String v = queue.dequeue();
-            for (String w : G.adj(v)) {
-                if (!dist.contains(w)) {
-                    queue.enqueue(w);
-                    dist.put(w, 1 + dist.get(v));
-                    prev.put(w, v);
-                }
-            }
+    while (!queue.isEmpty()) {
+      String v = queue.dequeue();
+      for (String w : G.adj(v)) {
+        if (!dist.contains(w)) {
+          queue.enqueue(w);
+          dist.put(w, 1 + dist.get(v));
+          prev.put(w, v);
         }
+      }
+    }
+  }
+
+  public boolean hasPathTo(String v) {
+    return dist.contains(v);
+  }
+  
+  public int distanceTo(String v) {
+    if (!hasPathTo(v))
+      return Integer.MAX_VALUE;
+    return dist.get(v);
+  }
+
+  public Iterable<String> pathTo(String v) {
+    Stack<String> path = new Stack<String>();
+    while (v != null && dist.contains(v)) {
+      path.push(v);
+      v = prev.get(v);
+    }
+    return path;
+  }
+
+  public static void main(String[] args) {
+    // create graph
+    Graph G = new Graph();
+    while (!StdIn.isEmpty()) {
+      String v = StdIn.readString();
+      String w = StdIn.readString();
+      G.addEdge(v, w);
     }
 
-    // is v reachable from the source s?
-    public boolean hasPathTo(String v) {
-        return dist.contains(v);
+    String s = args[0];
+    BreadthFirstSearch pf = new BreadthFirstSearch(G, s);
+    for (String t : G.vertices()) {
+      StdOut.println(t + "---");
+      for (String v : pf.pathTo(t)) {
+        StdOut.println("   " + v);
+      }
+      StdOut.println("distance " + pf.distanceTo(t));
     }
-
-    // return the length of the shortest path from v to s
-    public int distanceTo(String v) {
-        if (!hasPathTo(v)) return Integer.MAX_VALUE;
-        return dist.get(v);
-    }
-
-    // return the shortest path from v to s as an Iterable
-    public Iterable<String> pathTo(String v) {
-        Stack<String> path = new Stack<String>();
-        while (v != null && dist.contains(v)) {
-            path.push(v);
-            v = prev.get(v);
-        }
-        return path;
-    }
-
-
-    public static void main(String[] args) {
-        // create graph
-        Graph G = new Graph();
-        while (!StdIn.isEmpty()) {
-            String v = StdIn.readString();
-            String w = StdIn.readString();
-            G.addEdge(v, w);
-        }
-
-        String s = args[0];
-        BreadthFirstSearch pf = new BreadthFirstSearch(G, s);
-        for (String t : G.vertices()) {
-            StdOut.println(t + "---");
-            for (String v : pf.pathTo(t)) {
-                StdOut.println("   " + v);
-            }
-            StdOut.println("distance " + pf.distanceTo(t));
-        }
-    }
-
-
+  }
 }
--- ID1020/4/java/6/SymbolDigraph.java
+++ ID1020/4/java/6/SymbolDigraph.java
@@ -1,134 +1,65 @@
 import edu.princeton.cs.algs4.*;
 
 public class SymbolDigraph {
-    private ST<String, Integer> st;  // string -> index
-    private String[] keys;           // index  -> string
-    private Digraph graph;           // the underlying digraph
+  private ST<String, Integer> st;
+  private String[] keys;
+  private Digraph graph;
 
-    public SymbolDigraph(String filename, String delimiter) {
-        st = new ST<String, Integer>();
+  public SymbolDigraph(String filename, String delimiter) {
+    st = new ST<String, Integer>();
 
-        // First pass builds the index by reading strings to associate
-        // distinct strings with an index
-        In in = new In(filename);
-        
-        while (!in.isEmpty()) {
-            String[] a = in.readLine().split(delimiter);
-            String v = a[0];
-            String w = a[1];
-            if (!st.contains(v))
-              st.put(v, st.size());
-            if (!st.contains(w))
-              st.put(w, st.size());
-        }
+    In in = new In(filename);
 
-        // inverted index to get string keys in an array
-        keys = new String[st.size()];
-        for (String name : st.keys()) {
-            keys[st.get(name)] = name;
-        }
-
-        // second pass builds the digraph by connecting first vertex on each
-        // line to all others
-        graph = new Digraph(st.size());
-        in = new In(filename);
-        while (in.hasNextLine()) {
-            String[] a = in.readLine().split(delimiter);
-            int v = st.get(a[0]);
-            for (int i = 1; i < a.length; i++) {
-                int w = st.get(a[i]);
-                graph.addEdge(v, w);
-            }
-        }
+    while (!in.isEmpty()) {
+      String[] a = in.readLine().split(delimiter);
+      String v = a[0];
+      String w = a[1];
+      if (!st.contains(v))
+        st.put(v, st.size());
+      if (!st.contains(w))
+        st.put(w, st.size());
     }
 
-    /**
-     * Does the digraph contain the vertex named {@code s}?
-     * @param s the name of a vertex
-     * @return {@code true} if {@code s} is the name of a vertex, and {@code false} otherwise
-     */
-    public boolean contains(String s) {
-        return st.contains(s);
+    keys = new String[st.size()];
+    for (String name : st.keys()) {
+      keys[st.get(name)] = name;
     }
 
-    /**
-     * Returns the integer associated with the vertex named {@code s}.
-     * @param s the name of a vertex
-     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex named {@code s}
-     * @deprecated Replaced by {@link #indexOf(String)}.
-     */
-    @Deprecated
-    public int index(String s) {
-        return st.get(s);
+    graph = new Digraph(st.size());
+    in = new In(filename);
+    while (in.hasNextLine()) {
+      String[] a = in.readLine().split(delimiter);
+      int v = st.get(a[0]);
+      for (int i = 1; i < a.length; i++) {
+        int w = st.get(a[i]);
+        graph.addEdge(v, w);
+      }
     }
+  }
 
-    /**
-     * Returns the integer associated with the vertex named {@code s}.
-     * @param s the name of a vertex
-     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex named {@code s}
-     */
-    public int indexOf(String s) {
-        return st.get(s);
-    }
+  public boolean contains(String s) {
+    return st.contains(s);
+  }
 
-    /**
-     * Returns the name of the vertex associated with the integer {@code v}.
-     * @param  v the integer corresponding to a vertex (between 0 and <em>V</em> - 1) 
-     * @return the name of the vertex associated with the integer {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     * @deprecated Replaced by {@link #nameOf(int)}.
-     */
-    @Deprecated
-    public String name(int v) {
-        validateVertex(v);
-        return keys[v];
-    }
+  public int indexOf(String s) {
+    return st.get(s);
+  }
 
-    /**
-     * Returns the name of the vertex associated with the integer {@code v}.
-     * @param  v the integer corresponding to a vertex (between 0 and <em>V</em> - 1) 
-     * @return the name of the vertex associated with the integer {@code v}
-     * @throws IllegalArgumentException unless {@code 0 <= v < V}
-     */
-    public String nameOf(int v) {
-        validateVertex(v);
-        return keys[v];
-    }
+  public String nameOf(int v) {
+    validateVertex(v);
+    return keys[v];
+  }
 
-    /**
-     * Returns the digraph assoicated with the symbol graph. It is the client's responsibility
-     * not to mutate the digraph.
-     *
-     * @return the digraph associated with the symbol digraph
-     * @deprecated Replaced by {@link #digraph()}.
-     */
-    @Deprecated
-    public Digraph G() {
-        return graph;
-    }
+  public Digraph digraph() {
+    return graph;
+  }
 
-    /**
-     * Returns the digraph assoicated with the symbol graph. It is the client's responsibility
-     * not to mutate the digraph.
-     *
-     * @return the digraph associated with the symbol digraph
-     */
-    public Digraph digraph() {
-        return graph;
-    }
+  private void validateVertex(int v) {
+    int V = graph.V();
+    if (v < 0 || v >= V)
+      throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+  }
 
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        int V = graph.V();
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
-    }
-
-    /**
-     * Unit tests the {@code SymbolDigraph} data type.
-     *
-     * @param args the command-line arguments
-     */
   public static void main(String[] args) {
     String filename  = args[0];
     String delimiter = args[1];
@@ -138,39 +69,13 @@ public class SymbolDigraph {
     DirectedCycle dc = new DirectedCycle(graph);
     
     if (dc.hasCycle()) {
-        StdOut.print("Directed cycle: ");
-        for (int v : dc.cycle()) {
-            StdOut.print(sg.nameOf(v) + " ");
-        }
-        StdOut.println();
-    }
-
-    else {
-        StdOut.println("No directed cycle");
+      StdOut.print("Directed cycle: ");
+      for (int v : dc.cycle()) {
+        StdOut.print(sg.nameOf(v) + " ");
+      }
+      StdOut.println();
+    } else {
+      StdOut.println("No directed cycle");
     }
   }
 }
-
-/******************************************************************************
- *  Copyright 2002-2018, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
